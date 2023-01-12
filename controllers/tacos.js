14c72a873f3570f58319c51a3fa9d41e147bbd2a
gitpichardo <aopichar@gmail.com>
2023-01-12 06:36:42 -0800

    implement adding tacos feature

--- controllers/tacos.js
+++ controllers/tacos.js
@@ -15,6 +15,24 @@ function index(req, res) {
     })
   }
 
+  function create(req, res) {
+    //make sure to console.log(req.body) and console.log(req.user, "THE USER MAKING THE REQUEST")
+    //ask what is being returned on req.body > should be the name: "whatever name is" tasty: "either on or shows nothing"
+    req.body.owner = req.user.profile._id
+    //console.log(req.body) again
+    //ask before adding this code
+    req.body.tasty = !!req.body.tasty
+    Taco.create(req.body)
+    .then(taco => {
+      res.redirect('/tacos')
+    })
+    .catch(err => {
+      console.log(err)
+      res.redirect('/tacos')
+    })
+  }
+
 export {
   index,
+  create
 }
--- routes/tacos.js
+++ routes/tacos.js
@@ -1,10 +1,14 @@
 import { Router } from 'express'
 import * as tacosCtrl from '../controllers/tacos.js'
+import { isLoggedIn } from '../middleware/middleware.js'
 
 const router = Router()
 
 // Get /tacos
 router.get('/', tacosCtrl.index)
+
+//Post /tacos
+router.post('/', isLoggedIn, tacosCtrl.create)
 
 export {
   router
